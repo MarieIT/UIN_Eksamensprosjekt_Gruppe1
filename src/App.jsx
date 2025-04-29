ef41f3393dd1957df2c5678d14fe619e9be91e81
Lubbsaur <berghjunior@gmail.com>
2025-04-29 11:43:49 +0200

    La til Layout.jsx og routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom';
+import EventPage from './components/EventPage'
+import Home from './components/Home'
+import CategoryPage from './components/CategoryPage'
+import Dashboard from './components/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/event/:id' element={<EventPage />}/>
+        <Route path='/category/:slug' element={<CategoryPage />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+      </Routes>
+    </Layout>
   )
 }
 

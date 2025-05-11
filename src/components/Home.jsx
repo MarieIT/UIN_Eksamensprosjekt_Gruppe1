3819723b6af55dec0b4235e46f06c2b14c98e683
Lubbsaur <berghjunior@gmail.com>
2025-05-11 16:31:18 +0200

    Bleh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,19 @@ import '../styles/home.scss'
 export default function Home({ discovery, setApi }) {
   const [eventContent, setEventContent] = useState();
   const [cityContent, setCityContent] = useState();
+  const [cityName, setCityName] = useState("Oslo");
+  const [testCityName, setTestCityName] = useState();
 
   //Findings ID: K8vZ917K7fV
   //Tons ID: K8vZ917oWOV
   //Neon ID: K8vZ917_YJf
   //Skeikampen ID: K8vZ917bJC7
+
+  function CityNameFromClick(input) {
+    setCityName(input.target.innerHTML);
+    console.log(cityName);
+    CheckCityName(cityName);
+  }
 
   useEffect(() => {
     fetch('https://app.ticketmaster.com/discovery/v2/attractions?apikey=LWeeRs6C0ToGwEe5Gz96AnZM9scR2ynq&id=K8vZ917K7fV,%20K8vZ917oWOV,%20K8vZ917_YJf,%20K8vZ917bJC7&locale=*')
@@ -21,20 +29,21 @@ export default function Home({ discovery, setApi }) {
       setEventContent(data);
     })  
   }, [])
-
+  
   useEffect(() => {
-    fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=LWeeRs6C0ToGwEe5Gz96AnZM9scR2ynq&locale=*&city=oslo,%20paris,%20stockholm,%20berlin,%20london')
+    fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=LWeeRs6C0ToGwEe5Gz96AnZM9scR2ynq&locale=*&city=${cityName}`)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data, "fra Home ");
+      console.log(data, "fra Home – byer");
       setCityContent(data);
     })  
   }, [])
 
-  console.log(eventContent, "fra eventContent")
-  console.log(cityContent, "Fra cityContent")
+  console.log(cityName, "cityName");
+  console.log(cityContent, "cityContent");
+  
 
   return (
     <>
@@ -52,15 +61,26 @@ export default function Home({ discovery, setApi }) {
     <section className="by-knapper">
       <h2>Hva skjer i verdens storbyer?</h2>
       <article>
-        <Link>Oslo</Link>
-        <Link>Stockholm</Link>
-        <Link>Berlin</Link>
-        <Link>London</Link>
-        <Link>Paris</Link>
+        <Link onClick={CityNameFromClick}>Oslo</Link>
+        <Link onClick={CityNameFromClick}>Stockholm</Link>
+        <Link onClick={CityNameFromClick}>Berlin</Link>
+        <Link onClick={CityNameFromClick}>London</Link>
+        <Link onClick={CityNameFromClick}>Paris</Link>
       </article>
     </section>    
     <section>
-      {cityContent?._embedded.events.
+      {cityContent?._embedded.events.map((city) =>
+        <article key={city.id}>
+        <h3>{city.name}</h3>
+        </article>
+      )}
+    </section>
+    </>
+  )
+}
+
+/*
+{cityContent?._embedded.events.
         map((cityEvent) => <article key={cityEvent.id}>
           <img src={cityEvent.images.
             filter(image => image.width > 1000)[0].url}/>
@@ -71,7 +91,4 @@ export default function Home({ discovery, setApi }) {
           <p>{cityEvent._embedded.venues[0].country.name}</p>
           <p>{cityEvent._embedded.venues[0].name}</p>
       </article>)}
-    </section>
-    </>
-  )
-}
+*/

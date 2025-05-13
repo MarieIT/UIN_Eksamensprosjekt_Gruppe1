16729a0ea2bfb772658eed09ec86b6329e1fd5e5
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2025-05-13 16:22:21 +0200

    Added some changes to fetching eventpage

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,28 +1,28 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
+import EventCard from "./EventCard"
 
 export default function EventPage({ discovery, setApi }) {
   const {id} = useParams()
-  const [event, setEvent] = useState()
+  const [events, setEvents] = useState()
+
+  const getEvents = async () => {
+    fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=LWeeRs6C0ToGwEe5Gz96AnZM9scR2ynq&attractionId=${id}&locale=*`)
+    .then((response) => response.json())
+    .then((data) => setEvents(data._embedded.events))
+    .catch((error) => console.error("Fetching failed ", error))
+  }
 
   useEffect(() => {
-      fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=LWeeRs6C0ToGwEe5Gz96AnZM9scR2ynq&id=${id}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data, "event page ");
-        setEvent(data._embedded.events[0]);
-      })  
+      getEvents()
+      console.log(events, "Event-page")
     }, [])
 
   return (
     <>
-      <h1>{event?.name}</h1>
-      <section>
-        <ul>
-        </ul>
-      </section>
+      <h1>{/** */}</h1>
+      {events?.map((event) => <EventCard key={event.id} event={event}/>)}
+      {/*events[0]?._embedded.attractions.map()*/}
     </>
   )
 }

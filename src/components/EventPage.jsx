5e1a60efed9d960ff6adb60767eb07f79d854b1f
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2025-05-14 13:11:30 +0200

    Storing of artist and mapping out ArtistCard done

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,15 +1,17 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import EventCard from "./EventCard"
+import ArtistCard from "./ArtistCard"
 
 export default function EventPage({ discovery, setApi }) {
   const {id} = useParams()
   const [events, setEvents] = useState()
+  const [artists, setArtists] = useState()
 
   const getEvents = async () => {
     fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=LWeeRs6C0ToGwEe5Gz96AnZM9scR2ynq&attractionId=${id}&locale=*`)
     .then((response) => response.json())
-    .then((data) => setEvents(data._embedded.events))
+    .then((data) => {setEvents(data._embedded.events); setArtists(data._embedded.events[0]._embedded.attractions)})
     .catch((error) => console.error("Fetching failed ", error))
   }
 
@@ -22,7 +24,7 @@ export default function EventPage({ discovery, setApi }) {
     <>
       <h1>{/** */}</h1>
       {events?.map((event) => <EventCard key={event.id} event={event}/>)}
-      {/*events[0]?._embedded.attractions.map()*/}
+      {artists?.map((artist) => <ArtistCard artist={artist}/>)}
     </>
   )
 }

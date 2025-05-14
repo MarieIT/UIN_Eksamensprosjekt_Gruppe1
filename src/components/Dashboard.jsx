6d38074293a60df1981d6e88d684924ffae2b890
MarieIT <142742570+MarieIT@users.noreply.github.com>
2025-05-14 15:29:12 +0200

    Lagt til mer sass på dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import "../styles/dashboard.scss"
 
 export default function Dashboard({handleClick}) {
   const [user, setUser] = useState()
